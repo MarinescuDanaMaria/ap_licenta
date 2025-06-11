96cea5f14c51d66fa7518ab15a6d06e0ff7de8df
MarinescuDanaMaria <160782478+MarinescuDanaMaria@users.noreply.github.com>
2025-06-11 14:31:41 +0300

    Fix Firebase Auth import and initialization

--- config/firebaseConfig.jsx
+++ config/firebaseConfig.jsx
@@ -1,7 +1,7 @@
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-import * as firebaseAuth from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -17,8 +17,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = firebaseAuth.initializeAuth(app, {
-  persistence: firebaseAuth.getReactNativePersistence(ReactNativeAsyncStorage)
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
 
 export const db = getFirestore(app);
